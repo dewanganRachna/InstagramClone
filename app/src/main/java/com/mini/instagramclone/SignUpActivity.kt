7ab888e0ac3a40c523b06575b00110bfde7ea5be
rachna <rachnadewangan92@gmail.com>
2023-10-27 18:51:52 +0530

    Updated

--- app/src/main/java/com/mini/instagramclone/SignUpActivity.kt
+++ app/src/main/java/com/mini/instagramclone/SignUpActivity.kt
@@ -2,10 +2,27 @@ package com.mini.instagramclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.mini.instagramclone.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    val binding by lazy{
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        binding.signUpButton.setOnClickListener {
+                if (binding.name.editText?.text.toString().equals("") or
+                    binding.email.editText?.text.toString().equals("") or
+                    binding.password.editText?.text.toString().equals("")
+                ) {
+
+                    Toast.makeText(this@SignUpActivity, "please fill the above details", Toast.LENGTH_SHORT).show()
+                } else {
+                }
+            }
+        }
     }
-}
